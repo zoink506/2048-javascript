eb7c0a69562c321901dd7a6064be78472897ce8a
zoink506 <benhubble03@gmail.com>
2022-12-11 19:12:54 +1100

    Create drawing function to update screen

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function startGame() {
   // Creates the games grid, adjust gridSize to change size of grid
-  const gameDiv = document.querySelector("#game");
+  const gameDiv = document.querySelector("#columns");
   const gridSize = 4;
   let gameGrid = new Array;
 
@@ -9,28 +9,44 @@ function startGame() {
     gameGrid[i] = row;
     for(let j = 0; j < gridSize; j++) {
       row[j] = 0;
-      const cellDiv = document.createElement("div");
-      cellDiv.id = `${i}-${j}`;
-      cellDiv.innerText = `${i}-${j}`;
-      gameDiv.append(cellDiv);
     }
   }
 
-  createEventListeners(gameDiv, gameGrid);
+  createEventListeners(gameGrid);
   console.log(gameGrid);
 }
 
-function createEventListeners(gameDiv, gameGrid) {
+function createEventListeners(gameGrid) {
   document.addEventListener("keydown", e => {
     key = e.key;
     if(key === "w" || key === "a" || key === "s" || key === "d" || key === "W" || key === "A" || key === "S" || key === "D") {
-      moveGrid(key);
+      moveGrid(gameGrid, key);
     }
   });
 }
 
-function moveGrid(direction) {
+function moveGrid(grid, direction) {
+  console.log(direction);
+
+  drawGrid(grid);
+}
+
+function drawGrid(grid) {
+  const gridSize = grid.length;
+  const columnDiv = document.querySelector("#columns");
+  const columns = document.getElementsByClassName("column");
   
+  console.log(columns);
+  console.log(columnDiv)
+
+  for(let i = 0; i < gridSize; i++) {
+    const row = grid[i];
+    
+    for(let j = 0; j < row.length; j++) {
+
+    }
+
+  }
 }
 
 startGame();
